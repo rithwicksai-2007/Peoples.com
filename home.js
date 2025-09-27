9c6734f2ef47613fcef7fda26c45a5324735a52a
rithwicksai-2007 <rithwicksai.2007@gmail.com>
2025-09-27 18:40:05 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('active');
+});
